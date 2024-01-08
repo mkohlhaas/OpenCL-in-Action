c886a0396662ab97b2c1b9cd04dd29536a3a24c0
Hans-J. Schmid <knock@myopendoor.de>
2024-01-08 05:53:34 +0100

    separate cmakes

--- Ch02/contextCount/aux.h
+++ Ch02/contextCount/aux.h
@@ -1,9 +1,10 @@
 #include <CL/cl.h>
 
-cl_context createContext(cl_device_id);
-cl_device_id getFirstGPUDevice(cl_platform_id);
-cl_platform_id getFirstPlatform();
-cl_uint getReferenceCount(cl_context);
-void releaseContext(cl_context context);
-void releaseResources(cl_context, cl_device_id);
-void retainContext(cl_context context);
+// clang-format off
+cl_context     createContext     (cl_device_id);
+cl_device_id   getFirstGPUDevice (cl_platform_id);
+cl_platform_id getFirstPlatform  ();
+cl_uint        getReferenceCount (cl_context);
+void           releaseContext    (cl_context context);
+void           releaseResources  (cl_context, cl_device_id);
+void           retainContext     (cl_context context);
--- Ch02/queue_kernel/blank.cl
+++ Ch02/queue_kernel/blank.cl
@@ -1,2 +1,1 @@
-kernel void blank() {
-}
+kernel void blank() { }
